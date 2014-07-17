26829832238dba67689d7fed5cc6e2197b8b59f8
Brandon Altvater <brandon.altvater@gmail.com>
2014-07-16 22:28:07 -0400

    Version 1.0.1 has been submitted to the App Store for Review

--- seagullcentury_iOS_v2/seagullcentury_iOS_v2/AppDelegate.m
+++ seagullcentury_iOS_v2/seagullcentury_iOS_v2/AppDelegate.m
@@ -33,8 +33,10 @@
 
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     [[UIApplication sharedApplication] setStatusBarHidden:NO];
+    
     [self.window makeKeyAndVisible];
     [[Harpy sharedInstance] setAppID:@"<#896362043#>"];
+    [[Harpy sharedInstance] setAppName:@"sea-gull-century"];
     [[Harpy sharedInstance] setAlertType:HarpyAlertTypeForce];
     
     return YES;
