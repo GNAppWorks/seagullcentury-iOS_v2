c0b5ea6645bfa40451a33ce615a516cefec9aaf3
Brandon Altvater <brandon.altvater@gmail.com>
2014-07-31 20:39:24 -0400

    Added Mapbox SDK to project

---
+++ SGC/Pods/Pods-SGC_Tests-SMCalloutView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SGC_Tests_SMCalloutView : NSObject
+@end
+@implementation PodsDummy_Pods_SGC_Tests_SMCalloutView
+@end
--- SGC/Pods/Pods-SGC_Tests-resources.sh
+++ SGC/Pods/Pods-SGC_Tests-resources.sh
@@ -43,6 +43,7 @@ install_resource()
 }
 install_resource "Harpy/Harpy/Harpy.bundle"
 install_resource "iRate/iRate/iRate.bundle"
+install_resource "${BUILT_PRODUCTS_DIR}/Mapbox.bundle"
 
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 if [[ "${ACTION}" == "install" ]]; then
