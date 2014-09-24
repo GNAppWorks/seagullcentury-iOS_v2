c6269d2f32f4a323501d391b9e0adf5b510fc9bb
Brandon Altvater <brandon.altvater@gmail.com>
2014-09-24 11:50:02 -0400

    Revert "Updated to fix minor spelling mistakes"
    This reverts commit ef9017d16be61374a3efe7063918eb169d54ecdb.

--- SGC/seagullcentury_iOS_v2/AppDelegate.m
+++ SGC/seagullcentury_iOS_v2/AppDelegate.m
@@ -62,7 +62,6 @@
     [[Harpy sharedInstance] setAppID:@"896362043"];
     [[Harpy sharedInstance] setAppName:@"Sea Gull Century"];
     [[Harpy sharedInstance] setAlertType:HarpyAlertTypeForce];
-    [[Harpy sharedInstance] checkVersion];
     
     
     
@@ -96,6 +95,7 @@
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
+    [[Harpy sharedInstance] checkVersionDaily];
     
 }
 
